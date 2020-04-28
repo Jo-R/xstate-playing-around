ce80b2cc41409f0907c5a0839f7b3fb2f01b7d2b
Jo-R <jo.rigg8@gmail.com>
2020-04-28 20:24:21 +0100

    messing around with colour and font

--- src/App.js
+++ src/App.js
@@ -4,6 +4,16 @@ import { Input } from "./components/Input";
 import { useMachine } from "@xstate/react";
 import { stateMachine } from "../src/stateMachine";
 import { userData } from "./dummyUserData";
+import styled from "styled-components";
+
+// TODO
+// - sort the colours into theme
+// - input field style
+// - pull stuff out to components
+// - build the reg form
+// - fail messgae if password fails
+// - loading spinner
+// - make the user check better (ie >1 user!)
 
 function App() {
   const [state, send] = useMachine(stateMachine);
@@ -20,7 +30,6 @@ function App() {
     send("CHECKEMAIL");
     setTimeout(() => {
       if (email === userData.users.email) {
-        // TODO these checks (this and pwd) only work for one user in obj
         send("EMAILFOUND");
       } else {
         send("EMAILNOTFOUND");
@@ -39,7 +48,7 @@ function App() {
       if (password === userData.users.password) {
         send("PASSWORDFOUND");
       } else {
-        send("PASSWORDNOTFOUND"); // TODO if fail need a message to say try again
+        send("PASSWORDNOTFOUND");
       }
     }, 1000);
   };
@@ -47,44 +56,74 @@ function App() {
   const registerHandler = () => {
     send("SUBMITREG");
     setTimeout(() => {
-      //TODO actually add user
       send("REGISTERED");
     }, 1000);
   };
 
   return (
     <>
-      {state.value === "loading" && <p>Loading...</p>}
-      {state.value === "awaiting" && (
-        <>
-          <Input
-            id="email"
-            labelText="Enter your email: "
-            onChange={handleEmailInputChange}
-          />
-          <button onClick={findEmailHandler}>Go</button>
-        </>
-      )}
+      <CenteredHeader>
+        <h1>Logging in with x-state</h1>
+      </CenteredHeader>
+      <LoginContainer>
+        {state.value === "loading" && <p>Loading...</p>}
+        {state.value === "awaiting" && (
+          <>
+            <Input
+              id="email"
+              labelText="Enter your email: "
+              onChange={handleEmailInputChange}
+            />
+            <StyledButton onClick={findEmailHandler}>Go</StyledButton>
+          </>
+        )}
 
-      {state.value === "password" && (
-        <>
-          <Input
-            id="password"
-            labelText="Enter password: "
-            onChange={handlePasswordInputChange}
-          />
-          <button onClick={checkPasswordHandler}>Enter</button>
-        </>
-      )}
-      {state.value === "register" && (
-        <>
-          <p>create a register form...</p>
-          <button onClick={registerHandler}>Submit</button>
-        </>
-      )}
-      {state.value === "loggedIn" && <h1>You're logged in</h1>}
+        {state.value === "password" && (
+          <>
+            <Input
+              id="password"
+              labelText="Enter password: "
+              onChange={handlePasswordInputChange}
+            />
+            <StyledButton onClick={checkPasswordHandler}>Enter</StyledButton>
+          </>
+        )}
+        {state.value === "register" && (
+          <>
+            <p>create a register form...</p>
+            <button onClick={registerHandler}>Submit</button>
+          </>
+        )}
+        {state.value === "loggedIn" && <h1>You're logged in</h1>}
+      </LoginContainer>
     </>
   );
 }
 
 export default App;
+
+const CenteredHeader = styled.div`
+  display: grid;
+  place-items: center;
+  padding-top: 3em;
+  font-family: "Audiowide", cursive;
+  color: #c78b8d;
+`;
+
+const LoginContainer = styled.div`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: #ac9fbb;
+  padding: 5em;
+  border-radius: 10px;
+  color: #1d1e2c;
+  box-shadow: 20px 20px 60px #625375, -20px -20px 60px #84719f;
+`;
+
+const StyledButton = styled.button`
+  background-color: #c78b8d;
+  color: #2d2d34;
+  padding: 3px 10px;
+`;
